0f1446a61b0c06c988f0a8851946f89b4cc448dd
Gil Creque <gil@codecraftworks.com>
2019-11-17 17:16:22 -0500

    WIP

--- src/components/instagram-posts.js
+++ src/components/instagram-posts.js
@@ -14,7 +14,27 @@ const InstagramPosts = props => {
       <h2>Instagram Posts</h2>
       {posts.map(({ node }, index) => (
         <article key={node.id}>
-          <h3>{entityToChar(node.caption)}</h3>
+          <h3>
+            {entityToChar(node.caption) ||
+              new Intl.DateTimeFormat("en-US", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              }).format(node.timestamp)}
+          </h3>
+          <img
+            fluid={node.localFile.childImageSharp.fluid}
+            alt={
+              entityToChar(node.caption) ||
+              new Intl.DateTimeFormat("en-US", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              }).format(node.timestamp)
+            }
+          />
         </article>
       ))}
     </>
